67fd1b1071f30b8af4be5f7ab494f7c3f9e7e3d6
Шапошников Денис <wokin.shopash@gmail.com>
2015-07-23 16:06:53 +0300

    start redesign

--- DIHT/apps/main/urls.py
+++ DIHT/apps/main/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+    url(r'^$', TemplateView.as_view(template_name="cabinet.html"), name='home'),
 ]
