d1d3f9704d3da50d2e589d751477ceead85e9e39
Шапошников Денис <wokin.shopash@gmail.com>
2015-08-02 21:50:46 +0300

    created a couple of static pages

--- DIHT/apps/main/urls.py
+++ DIHT/apps/main/urls.py
@@ -3,4 +3,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+    url(r'^gym/$', TemplateView.as_view(template_name="gym.html"), name='gym'),
+    url(r'^accommodation/$', TemplateView.as_view(template_name="accommodation.html"), name='accommodation'),
+    url(r'^announcements/$', TemplateView.as_view(template_name="announcements.html"), name='announcements'),
+	
 ]
