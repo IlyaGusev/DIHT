0eb94faf8ad05159cf261e8dcaa4520e58e46af9
Шапошников Денис <wokin.shopash@gmail.com>
2015-08-19 15:26:02 +0300

    new dash see #5

--- DIHT/apps/activism/urls.py
+++ DIHT/apps/activism/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     url(r'^tasks/(?P<pk>[0-9]*)/do$', DoTaskView.as_view(), name='do_task'),
     url(r'^tasks/(?P<pk>[0-9]*)/$', TaskView.as_view(), name='task'),
     url(r'^tasks/create/$', TaskCreateView.as_view(), name='create_task'),
-    url(r'^events_test/$', TemplateView.as_view(template_name="activism/events.html"), name='events'),
-	
 ]
