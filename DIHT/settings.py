205784b4ac4652c48f727c009ff3d37a4299ee1a
Ilya Gusev <ilya.gusev@phystech.edu>
2015-09-04 14:32:42 +0300

    Social auth on login page

--- DIHT/settings.py
+++ DIHT/settings.py
@@ -161,3 +161,13 @@ LOGGING = {
         },
     },
 }
+
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details'
+)
