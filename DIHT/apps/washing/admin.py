5e893bfbd9ccdd144ca30c0039f1e4b090f17bae
Ilya Gusev <ilya.gusev@phystech.edu>
2015-09-05 12:43:44 +0300

    Washing admin search

--- DIHT/apps/washing/admin.py
+++ DIHT/apps/washing/admin.py
@@ -7,13 +7,26 @@ def cancel_record_operation(modeladmin, request, queryset):
 
 cancel_record_operation.short_description = "Отмена записи с возвращением денег"
 
+
 class WashingMachineRecordAdmin(admin.ModelAdmin):
     model = WashingMachineRecord
     actions = [cancel_record_operation]
+    search_fields = ['user__first_name', 'user__last_name', 'datetime_from', 'machine__name']
+
+
+def full_name(obj):
+    return u"%s %s" % (obj.user.first_name, obj.user.last_name)
+
+
+class BlackListRecordAdmin(admin.ModelAdmin):
+    model = BlackListRecord
+    list_display = (full_name, 'user')
+    search_fields = ['user__first_name', 'user__last_name']
+
 
 admin.site.register(WashingMachineRecord, WashingMachineRecordAdmin)
 admin.site.register(WashingMachine)
 admin.site.register(NonWorkingDay)
 admin.site.register(RegularNonWorkingDay)
 admin.site.register(Parameters)
-admin.site.register(BlackListRecord)
+admin.site.register(BlackListRecord, BlackListRecordAdmin)
