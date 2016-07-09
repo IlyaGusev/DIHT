5265126f42cb357a2cb4e2f1086d0e63dc5cac22
Ilya Gusev <phoenixilya@gmail.com>
2016-07-09 04:30:29 +0300

    Принуждаю на некоторых страницах спрашивать логин вместо выбрасывания 403 close #103

--- DIHT/apps/activism/urls.py
+++ DIHT/apps/activism/urls.py
@@ -8,6 +8,7 @@
         URL resolver модуля activism.
 """
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from activism.views import EventView, TaskView, IndexView, TaskCreateView, \
     EventsView, EventCreateView, TaskActionView, EventCloseView, UnlockView, \
     SectorView, ActivistsView, TaskLogView, AddPointsView, DeletePointsView, \
@@ -20,13 +21,13 @@ urlpatterns = [
 
     url(r'^events/$', EventsView.as_view(), name='events'),
     url(r'^events/create/$', EventCreateView.as_view(), name='create_event'),
-    url(r'^events/(?P<pk>[0-9]*)/$', EventView.as_view(), name='event'),
+    url(r'^events/(?P<pk>[0-9]*)/$', login_required(EventView.as_view()), name='event'),
     url(r'^events/(?P<pk>[0-9]*)/close/$', EventCloseView.as_view(), name='event_close'),
     url(r'^events/(?P<pk>[0-9]*)/best/$', EventBestView.as_view(), name='event_best'),
 
     url(r'^sectors/(?P<pk>[0-9]*)/$', SectorView.as_view(), name='sector'),
 
-    url(r'^tasks/(?P<pk>[0-9]*)/$', TaskView.as_view(), name='task'),
+    url(r'^tasks/(?P<pk>[0-9]*)/$', login_required(TaskView.as_view()), name='task'),
     url(r'^tasks/(?P<pk>[0-9]*)/log/$', TaskLogView.as_view(), name='task_log'),
     url(r'^tasks/(?P<pk>[0-9]*)/(?P<action>[0-9_a-z]+)/$', TaskActionView.as_view(), name='task_action'),
     url(r'^tasks/create/$', TaskCreateView.as_view(), name='create_task'),
